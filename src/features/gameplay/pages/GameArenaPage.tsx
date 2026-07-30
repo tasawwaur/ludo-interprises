c9b9bb20bfd9f9643c8f17a97c92d270ec2239d0
Ludo Developer <developer@ludoenterprise.internal>
2026-07-30 21:49:38 +0530

    style: position Luxury Chat emblem button on full right (bottom-8 right-3)

--- src/features/gameplay/pages/GameArenaPage.tsx
+++ src/features/gameplay/pages/GameArenaPage.tsx
@@ -150,8 +150,8 @@ export const GameArenaPage: React.FC<GameArenaPageProps> = ({ onLeaveGame, onSho
           </div>
         </div>
 
-        {/* LUXURY CHAT BUTTON: FIXED BOTTOM LEFT */}
-        <div className="absolute bottom-8 left-3 z-30 flex items-center justify-center">
+        {/* LUXURY CHAT BUTTON: FIXED BOTTOM RIGHT */}
+        <div className="absolute bottom-8 right-3 z-30 flex items-center justify-center">
           <button
             onClick={() => setShowChatModal(true)}
             className="w-14 h-14 relative hover:scale-110 active:scale-90 transition-transform cursor-pointer filter drop-shadow-[0_8px_16px_rgba(234,179,8,0.7)]"

4dec6bae490df859e6c1a3791416daff75bc969e
Ludo Developer <developer@ludoenterprise.internal>
2026-07-31 00:32:51 +0530

    Add 3D Luxury Pawns for Red, Green, Blue, and Yellow

--- src/features/gameplay/pages/GameArenaPage.tsx
+++ src/features/gameplay/pages/GameArenaPage.tsx
@@ -178,6 +178,33 @@ export const GameArenaPage: React.FC<GameArenaPageProps> = ({ onLeaveGame, onSho
             <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 8" className="w-full h-full object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)]" />
           </div>
 
+          {/* 3D LUXURY PAWNS DEMO IN 4 HOUSES */}
+          {/* Red House Pawns */}
+          <div className="absolute top-[20.5%] left-[20.5%] -translate-x-1/2 -translate-y-1/2 w-[22px] h-[28px] z-30 pointer-events-none filter drop-shadow-[0_4px_8px_rgba(0,0,0,0.8)]">
+            <img src="/assets/images/icons/token_red_3d.png" alt="Red Token 1" className="w-full h-full object-contain" />
+          </div>
+          <div className="absolute top-[20.5%] left-[32.5%] -translate-x-1/2 -translate-y-1/2 w-[22px] h-[28px] z-30 pointer-events-none filter drop-shadow-[0_4px_8px_rgba(0,0,0,0.8)]">
+            <img src="/assets/images/icons/token_red_3d.png" alt="Red Token 2" className="w-full h-full object-contain" />
+          </div>
+
+          {/* Yellow House Pawns */}
+          <div className="absolute top-[20.5%] left-[67.5%] -translate-x-1/2 -translate-y-1/2 w-[22px] h-[28px] z-30 pointer-events-none filter drop-shadow-[0_4px_8px_rgba(0,0,0,0.8)]">
+            <img src="/assets/images/icons/token_yellow_3d.png" alt="Yellow Token 1" className="w-full h-full object-contain" />
+          </div>
+          <div className="absolute top-[20.5%] left-[79.5%] -translate-x-1/2 -translate-y-1/2 w-[22px] h-[28px] z-30 pointer-events-none filter drop-shadow-[0_4px_8px_rgba(0,0,0,0.8)]">
+            <img src="/assets/images/icons/token_yellow_3d.png" alt="Yellow Token 2" className="w-full h-full object-contain" />
+          </div>
+
+          {/* Green House Pawns */}
+          <div className="absolute top-[67.5%] left-[20.5%] -translate-x-1/2 -translate-y-1/2 w-[22px] h-[28px] z-30 pointer-events-none filter drop-shadow-[0_4px_8px_rgba(0,0,0,0.8)]">
+            <img src="/assets/images/icons/token_green_3d.png" alt="Green Token 1" className="w-full h-full object-contain" />
+          </div>
+
+          {/* Blue House Pawns */}
+          <div className="absolute top-[67.5%] left-[79.5%] -translate-x-1/2 -translate-y-1/2 w-[22px] h-[28px] z-30 pointer-events-none filter drop-shadow-[0_4px_8px_rgba(0,0,0,0.8)]">
+            <img src="/assets/images/icons/token_blue_3d.png" alt="Blue Token 1" className="w-full h-full object-contain" />
+          </div>
+
           {/* Top Center Green Royal Frame & Opponent 3D Dice */}
           <div className="absolute -top-[20px] left-1/2 -translate-x-1/2 w-[84px] h-[96px] z-50 flex items-center justify-center pointer-events-auto">
             <img

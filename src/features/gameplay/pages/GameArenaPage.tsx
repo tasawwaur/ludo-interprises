cf7f41a5e7e52f583bc312f549d8471f56942cdb
Ludo Developer <developer@ludoenterprise.internal>
2026-07-30 23:51:56 +0530

    Overlay 8 3D Luxury Star icons directly on 3D Ludo board safe stops

--- src/features/gameplay/pages/GameArenaPage.tsx
+++ src/features/gameplay/pages/GameArenaPage.tsx
@@ -137,6 +137,27 @@ export const GameArenaPage: React.FC<GameArenaPageProps> = ({ onLeaveGame, onSho
             />
           </div>
 
+          {/* 8 SAFE STOPS 3D LUXURY STAR ICONS OVER 3D BOARD */}
+          {/* Green Start (Top-Left Track Entry) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 1" className="absolute top-[40.5%] left-[8%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+          {/* Green Star (Top-Left Track Star) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 2" className="absolute top-[52%] left-[20%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+
+          {/* Yellow Start (Top-Right Track Entry) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 3" className="absolute top-[8%] left-[58.5%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+          {/* Yellow Star (Top-Right Track Star) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 4" className="absolute top-[20.5%] left-[48%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+
+          {/* Blue Start (Bottom-Right Track Entry) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 5" className="absolute top-[58.5%] right-[8%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+          {/* Blue Star (Bottom-Right Track Star) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 6" className="absolute top-[47%] right-[20%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+
+          {/* Red Start (Bottom-Left Track Entry) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 7" className="absolute bottom-[8%] left-[40.5%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+          {/* Red Star (Bottom-Left Track Star) */}
+          <img src="/assets/images/icons/luxury_star_icon.png" alt="Star 8" className="absolute bottom-[20.5%] left-[48%] w-5 h-5 object-contain filter drop-shadow-[0_2px_8px_rgba(234,179,8,0.95)] z-25 pointer-events-none" />
+
           {/* Top Center Green Royal Frame & Opponent 3D Dice */}
           <div className="absolute -top-[20px] left-1/2 -translate-x-1/2 w-[84px] h-[96px] z-50 flex items-center justify-center pointer-events-auto">
             <img
